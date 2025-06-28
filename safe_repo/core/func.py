0466600a468e0d53aceedd50280d8c068572d8b4
KX3M <163962831+KX3M@users.noreply.github.com>
2025-06-28 12:08:22 +0530

    Update func.py

--- safe_repo/core/func.py
+++ safe_repo/core/func.py
@@ -36,10 +36,10 @@ async def subscribe(app, message):
       try:
          user = await app.get_chat_member(update_channel, message.from_user.id)
          if user.status == "kicked":
-            await message.reply_text("You are Banned. Contact -- @safe_repo")
+            await message.reply_text("You are Banned. Contact -- @metaui")
             return 1
       except UserNotParticipant:
-         await message.reply_photo(photo="https://graph.org/file/d44f024a08ded19452152.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"{url}")]]))
+         await message.reply_photo(photo="https://graph.org/file/e37f781b1c78b25eea7a8-9b77eb226807f3f282.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now...", url=f"{url}")]]))
          return 1
       except Exception:
          await message.reply_text("Something Went Wrong. Contact us @safe_repo...")
